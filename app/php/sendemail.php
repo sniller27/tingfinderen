d23094147eb26437b750ac1ebd0eedba80e9198c
Jonas <sniller27@hotmail.com>
2015-12-10 14:03:36 +0100

    test

--- app/php/sendemail.php
+++ app/php/sendemail.php
@@ -15,7 +15,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.live.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'heregoesmail@mailcom';                 // SMTP username
-$mail->Password = 'passsssword';                           // SMTP password
+$mail->Password = 'kagemand';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
