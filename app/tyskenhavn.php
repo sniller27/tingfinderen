950b38ccc12d9f918925f43e85d23c7ac418e9eb
Jonas <sniller27@hotmail.com>
2015-12-19 01:34:30 +0100

    tyskenhavn made + grid change

--- app/tyskenhavn.php
+++ app/tyskenhavn.php
@@ -42,22 +42,9 @@ session_start();
     </head>
 
     <body id="tyskenhavnbody">
-
-       <!--   facebook feed SDK-->
-            <div id="fb-root"></div>
-            <script>
-                (function (d, s, id) {
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s);
-                    js.id = id;
-                    js.src = "//connect.facebook.net/da_DK/sdk.js#xfbml=1&version=v2.5";
-                    fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));
-            </script>
        
        <!--   header-->
-    <div class="navbar navbar-default navbar-static-top">
+    <div class="navbar navbar-default navbar-static-top" id="tyskenhavnheader">
 
 
         <div class="container">
@@ -65,14 +52,14 @@ session_start();
 
             <div class="navbar-header">
 
-                <a href="index.php"><img src="images/tyskenhavn/tyskenhavnlogo.png" alt="Tingfinderen logo" id="logo"></a>
+                <a href="tyskenhavn.php"><img src="images/tyskenhavn/tyskenhavnlogo.png" alt="Tingfinderen logo" id="tyskenhavnlogo"></a>
 
                 <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse" id="nav-icon4">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <p class="cartamount" id="notification"><?= $itemcount ?></p>
+                <p class="cartamount" id="notification">0</p>
             </div>
 
             <!--mobile menu-->
@@ -81,11 +68,11 @@ session_start();
                 <ul class="nav navbar-nav navbar-right verticalalign">
 
 
-                    <li><a href="#" class="uppercase hvr-underline-from-center">Forside</a></li>
-                    <li><a href="#" class="uppercase hvr-underline-from-center">Shop</a></li>
-                    <li><a href="#" class="uppercase hvr-underline-from-center">Info</a></li>
-                    <li><a href="#" class="uppercase hvr-underline-from-center">Kontakt</a></li>
-                    <li><a href="index.php" class="uppercase hvr-underline-from-center">Tingfinderen.dk</a></li>
+                    <li><a href="#" class="uppercase hvr-underline-from-center tyskenhavnhover">Forside</a></li>
+                    <li><a href="#" class="uppercase hvr-underline-from-center tyskenhavnhover">Shop</a></li>
+                    <li><a href="#" class="uppercase hvr-underline-from-center tyskenhavnhover">Info</a></li>
+                    <li><a href="#" class="uppercase hvr-underline-from-center tyskenhavnhover">Kontakt</a></li>
+                    <li><a href="index.php" class="uppercase hvr-underline-from-center tyskenhavnhover">Tingfinderen.dk</a></li>
                     
 <!--                    CART IN MENU-->
                     <li><a href="#"><i class="fa fa-shopping-cart" style="font-size:30px;"></i><p class="cartamount">0</p></a></li>
@@ -106,58 +93,44 @@ session_start();
             
 
             <div class="container">
-
-                <div class="jumbotron jumbotrontyskenhavn">
-
-
-                <div class="text-center">
-<!--
-                    <h1>Tyskenhavn</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ex nec leo lobortis mattis. Maecenas ac interdum dui, at fermentum leo. Ut quis maximus enim.</p>
--->
-<!--
-                    <a href="#" class="btn graybutton sharp">Køb</a>
-                    <a href="#" class="btn graybutton sharp">Sælg</a>
--->
+                <div class="row">
+                    <div class="col-xs-12">
+                           <img src="images/tyskenhavn/coverimage.png" alt="tyskenhavncoverimage" style="width:100%">
+                    </div>
                 </div>
-
-
-            </div>
-
-
                     <!-- main -->
                     <main>
 
                         <div class="row bot-buffer">
 
-                            <div class="col-sm-3">
+                            <div class="col-sm-3 bot-buffer">
 
-                                <img src="http://tyskenhavn.dk/images/stories/virtuemart/product/tucker.png" alt="1.step in proces" class="processicon" />
-                                <h2 class="text-center">Finder historie</h2>
+                                <img src="images/tyskenhavn/vin-flaske.svg" alt="1.step in proces" class="processicon" />
+                                <h2 class="text-center">Tuckerbox Primotivo</h2>
                                 <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.</p>
 
                             </div>
 
-                            <div class="col-sm-3">
+                            <div class="col-sm-3 bot-buffer">
 
-                                <img src="http://tyskenhavn.dk/images/stories/virtuemart/product/tucker.png" alt="2.step in proces" class="processicon" />
-                                <h2 class="text-center">Restaurerer</h2>
+                                <img src="images/tyskenhavn/vin-flaske.svg" alt="2.step in proces" class="processicon" />
+                                <h2 class="text-center">Chateau Fombrauge</h2>
                                 <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.</p>
 
                             </div>
 
-                            <div class="col-sm-3">
+                            <div class="col-sm-3 bot-buffer">
 
-                                <img src="http://tyskenhavn.dk/images/stories/virtuemart/product/tucker.png" alt="3.step in proces" class="processicon" />
-                                <h2 class="text-center">Sælger</h2>
+                                <img src="images/tyskenhavn/vin-flaske.svg" alt="3.step in proces" class="processicon" />
+                                <h2 class="text-center">Piccini Memoro Rosso</h2>
                                 <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.</p>
 
                             </div>
 
-                            <div class="col-sm-3">
+                            <div class="col-sm-3 bot-buffer">
 
-                                <img src="http://tyskenhavn.dk/images/stories/virtuemart/product/tucker.png" alt="4.step in proces" class="processicon" />
-                                <h2 class="text-center">Pynter</h2>
+                                <img src="images/tyskenhavn/vin-flaske.svg" alt="4.step in proces" class="processicon" />
+                                <h2 class="text-center">Piccini Memoro Bianco</h2>
                                 <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.</p>
 
                             </div>
@@ -175,38 +148,35 @@ session_start();
                             <div class="row bot-buffer">
                                 <div class="col-sm-12 top-buffer">
 
-<h2>Hvem er vi?</h2>
-    <p>Tingfinderen er en nyetableret virksomhed i Danmark med fokus på spændende highend genbrug. EST 2015. Vi fokuserer på indkøb af kommunalt affald vi forvandler til guld, antikke spisfindiheder bliver rekonstrueret til det moderne hjem eller helt nye designs bliver skabt komplet fra bunden. Vi henvender os ikke til den enkelte katergori, men breder os gerne ud og kan finde på hvad som helst. Om det er lamper, møbler, heste eller fly, det er ligemeget. Vores team kombinerer deres ekspertise og kreative tankeveje med innovativt håndværk for at leverer de bedste produkter, af allerbedste kvalitet.</p>
+<h2>Rødvin</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.</p>
                              
                              </div> 
                                </div>
                                
                             <div class="row">
                             
-                            <div class="col-sm-6 bot-buffer">
+                            <div class="col-sm-12 bot-buffer">
 
-<h2>Konceptet</h2>
-    <p>Tingfinderen er en nyetableret virksomhed i Danmark med fokus på spændende highend genbrug. EST 2015. Vi fokuserer på indkøb af kommunalt affald vi forvandler til guld, antikke spisfindiheder bliver rekonstrueret til det moderne hjem eller helt nye designs bliver skabt komplet fra bunden.</p>
+<h2>Hvidvin</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta convallis ultricies. Duis et tortor ullamcorper, aliquet tellus non, rutrum eros.</p>
                              
                              </div>
-                             
-                             <div class="col-sm-6 bot-buffer">
-
-                             
-                             </div>
+                            
                               
                                </div>
                                
-                               
+                                
                                
                                 </div>
 
 <!--                           facebook feed-->
-                            <div class="col-sm-3 facebookfeedbuffer">
+                            <div class="col-sm-3 bot-buffer top-buffer">
 
-                                <div class="fb-page" data-href="https://www.facebook.com/BJCtingfinderen-995619370494820/timeline" data-tabs="timeline" data-width="360" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
-
-                            </div>
+<h2>Rosévin</h2>
+    <p>Tingfinderen er en nyetableret virksomhed i Danmark med fokus på spændende highend genbrug. EST 2015. Vi fokuserer på indkøb af kommunalt affald vi forvandler til guld, antikke spisfindiheder bliver rekonstrueret til det moderne hjem eller helt nye designs bliver skabt komplet fra bunden.</p>
+                             
+                             </div>
 
                         </div>
 
@@ -218,23 +188,25 @@ session_start();
             
 <!--        footer-->
        
-    <div id="footer">
+    <div id="footer" class="tyskenhavnfooter">
 <div class="container">
     <div class="row">
 
         <div class="col-sm-3 footerspace">
            
             <p>Kontakt:
-                <br/>+45 60 15 85 75
-                <br/> kontakt@tingfinderen.dk
+                <br/>+45 21 33 32 09
+                <br/> kontakt@tyskenhavn.dk
             </p>
 
         </div>
         <div class="col-sm-3 footerspace">
 
-            <p>Adresser:
-                <br/> København K, Sdr. Jylland og Odense C.
-                <br/> Danmark
+            <p>Adresse:<br>
+            Tyskenhavn ApS<br>
+            Knabrostræde 25 kl.<br>
+            1210 København K<br>
+            Danmark
             </p>
 
         </div>
@@ -244,11 +216,11 @@ session_start();
         </div>
         <div class="col-sm-3">
 <p>Find os også på sociale medier</p>
-            <a href="#"><i class="fa fa-facebook-square" style="font-size:48px;"></i></a>
-            <a href="#"><i class="fa fa-twitter-square" style="font-size:48px;"></i></a>
-            <a href="#"><i class="fa fa-google-plus-square" style="font-size:48px;"></i></a>
-            <a href="#"><i class="fa fa-instagram" style="font-size:48px;"></i></a>
-            <a href="#"><i class="fa fa-youtube-square" style="font-size:48px;"></i></a>
+            <a href="#"><i class="fa fa-facebook-square tyskenhavnsocialmedia" style="font-size:48px;"></i></a>
+            <a href="#"><i class="fa fa-twitter-square tyskenhavnsocialmedia" style="font-size:48px;"></i></a>
+            <a href="#"><i class="fa fa-google-plus-square tyskenhavnsocialmedia" style="font-size:48px;"></i></a>
+            <a href="#"><i class="fa fa-instagram tyskenhavnsocialmedia" style="font-size:48px;"></i></a>
+            <a href="#"><i class="fa fa-youtube-square tyskenhavnsocialmedia" style="font-size:48px;"></i></a>
 
        <a href="#"><img src="images/trustpilot.png" id="trustpilot" alt="trustpilot"></a>
         </div>
